b6f81464b0243ea3977a6f4055d8a15f679b3ab9
jpcind <108848785+jpcind@users.noreply.github.com>
2022-08-24 10:33:16 -0700

    Create sum_double.py

---
+++ sum_double.py
@@ -0,0 +1,14 @@
+def sum_double(a,b):
+    if a == b:
+        return (a+b) * 2
+    else:
+        return a+b
+
+
+def main():
+    print(sum_double(1,2))
+    print(sum_double(3,2))
+    print(sum_double(2,2))
+
+
+main()
