e9f65d66a58957bf3ac7c7fa3c578d29df2299e1
jpcind <108848785+jpcind@users.noreply.github.com>
2022-08-24 10:23:42 -0700

    Create middle_way.py

---
+++ middle_way.py
@@ -0,0 +1,7 @@
+def middle_way(a,b):
+    new_array = []
+    a_halfway = len(a) / 2
+    b_halfway = len(b) / 2
+    new_array.append(a[a_halfway])
+    new_array.append(b[b_halfway])
+    return new_array
